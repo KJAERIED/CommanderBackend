09e02cc1f9305258790622e362b5c7c795b64505
KJAERIED <m.ragalli@ied.edu>
2025-05-07 09:21:20 +0200

    initial commit

---
+++ commander/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for commander project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'commander.settings')
+
+application = get_asgi_application()
